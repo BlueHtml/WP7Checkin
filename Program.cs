1b8f26e5209a329f34a39f5d212195bb59148e00
fly <luxiaofeng006@qq.com>
2023-01-04 16:58:04 +0800

    升级到net7

--- Program.cs
+++ Program.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
+﻿using System.Net;
 using System.Text;
 using System.Text.Json;
-using System.Threading.Tasks;
 
 namespace WP7Checkin
 {
